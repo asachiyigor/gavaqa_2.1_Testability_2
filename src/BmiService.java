7c97096f578aab40f17abce4418dff909f48c022
asachiyigor <asachiyigor@gmail.com>
2022-03-24 16:32:55 +0300

    version 2.1.2.2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,10 +1,7 @@
 public class BmiService {
     public static double calculate(int heightSm, int weightKg) {
 
-//        double heightSm = 0.16; // метры - рост
-//        double weightKg = 60.00; // килограммы - вес
-
-        double  heightM = (double) heightSm/1000;
+        double heightM = (double) heightSm / 1000;
         return ((weightKg / (Math.pow((heightM), 2))) / 100);
     }
 }
